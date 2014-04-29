a10593c7e73913d0537fc0650a747c6bfe280df2
dennispkramer <dennispkramer@gmail.com>
2014-04-29 11:12:14 -0400

    including jquery and working on matching heights on index

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,23 @@
 //= require_tree .
+
+$(document).ready(function() {
+
+  setHeight($('#master'), $('#child1'), $('#child2'));
+
+  // When the window is resized the height might
+  // change depending on content. So to be safe
+  // we rerun the function
+  $(window).on(resize, function() {
+      setHeight($('#master'), $('#child1'), $('#child2'));      
+  });
+
+});
+
+function setHeight(elem1, elem2, elem3) {
+  var height = elem1.height()
+  var doubleheight = 2 * height
+  // sets height of element 2 equal to the height of element 1
+  elem2.css('height', height);
+  // sets height of element 3 equal to 2X the height of element 1
+  elem3.css('height', doubleheight);
+}
