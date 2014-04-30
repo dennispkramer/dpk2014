a846501e93e54a513e717a98c4f36f98ad1f5202
dennispkramer <dennispkramer@gmail.com>
2014-04-30 11:15:19 -0400

    finalizing homepage assets

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,18 +2,18 @@
 
 $(document).ready(function() {
 
-  setHeight($('#aMaster'), $('#aChild1'), $('#aChild2'));
+  leadHeight($('#master'), $('#child1'), $('#child2'));
 
   // When the window is resized the height might
   // change depending on content. So to be safe
   // we rerun the function
   $(window).resize(function() {
-      setHeight($('#aMaster'), $('#aChild1'), $('#aChild2'));      
+      leadHeight($('#master'), $('#child1'), $('#child2'));      
   });
 
 });
 
-function setHeight(elem1, elem2, elem3) {
+function leadHeight(elem1, elem2, elem3) {
   var height = elem1.height() + 120
   var doubleheight = 2 * height
   // sets height of element 2 equal to the height of element 1
@@ -22,24 +22,26 @@ function setHeight(elem1, elem2, elem3) {
   elem3.css('min-height', doubleheight);
 }
 
+
+// sets height equal to width of 1/4 of browser
+// var galleryWidth = $('#gallery').width();
+// $('#gallery').css('height', galleryWidth);
+
 $(document).ready(function() {
 
-  setHeight($('#bMaster'), $('#bChild1'), $('#bChild2'));
+  galleryHeight($('#gallery'));
 
   // When the window is resized the height might
   // change depending on content. So to be safe
   // we rerun the function
   $(window).resize(function() {
-      setHeight($('#bMaster'), $('#bChild1'), $('#bChild2'));      
+      galleryHeight($('#gallery'));      
   });
 
 });
 
-function setHeight(elem1, elem2, elem3) {
-  var height = elem1.height() + 120
-  var doubleheight = 2 * height
-  // sets height of element 2 equal to the height of element 1
-  elem2.css('height', height);
-  // sets height of element 3 equal to 2X the height of element 1
-  elem3.css('min-height', doubleheight);
+function galleryHeight(elem1) {
+  var width = elem1.width() / 4
+  // sets height equal to width of 1/4 of browser
+  elem1.css('height', width);
 }
