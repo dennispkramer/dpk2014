d6ff3f614d551d77aa66103a1ac9354c641c3c90
dennispkramer <dennispkramer@gmail.com>
2014-05-01 08:43:15 -0400

    cleaning out code for gallery so no need for JS

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -21,27 +21,3 @@ function leadHeight(elem1, elem2, elem3) {
   // sets height of element 3 equal to 2X the height of element 1
   elem3.css('min-height', doubleheight);
 }
-
-
-// sets height equal to width of 1/4 of browser
-// var galleryWidth = $('#gallery').width();
-// $('#gallery').css('height', galleryWidth);
-
-$(document).ready(function() {
-
-  galleryHeight($('#gallery'));
-
-  // When the window is resized the height might
-  // change depending on content. So to be safe
-  // we rerun the function
-  $(window).resize(function() {
-      galleryHeight($('#gallery'));      
-  });
-
-});
-
-function galleryHeight(elem1) {
-  var width = elem1.width() / 4
-  // sets height equal to width of 1/4 of browser
-  elem1.css('height', width);
-}
